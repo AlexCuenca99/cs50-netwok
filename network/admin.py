f6f8e7fcad70dbcda65d2295419294b10637c3c6
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-25 20:35:55 -0500

    Add Post, Follow, Reaction

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Post, Follow, Reaction
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Follow)
+admin.site.register(Reaction)
--- network/models.py
+++ network/models.py
@@ -1,6 +1,63 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from model_utils.models import TimeStampedModel
+
 
 class User(AbstractUser):
     pass
+
+
+class Post(TimeStampedModel, models.Model):
+    author = models.ForeignKey(
+        "User",
+        verbose_name=("Post Author"),
+        related_name="authors",
+        on_delete=models.CASCADE,
+    )
+    content = models.TextField("Post Content", max_length=150)
+
+    def __str__(self):
+        return f"{self.author} - {self.created}"
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+
+class Reaction(TimeStampedModel, models.Model):
+    user = models.ForeignKey(
+        "User", verbose_name=("User who reacts"), on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(
+        "Post", verbose_name=("Liked Post"), on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.user} - {self.created}"
+
+    class Meta:
+        verbose_name = "Reaction"
+        verbose_name_plural = "Reactions"
+
+
+class Follow(TimeStampedModel, models.Model):
+    follower = models.ForeignKey(
+        "User",
+        verbose_name=("Follower"),
+        related_name="followers",
+        on_delete=models.CASCADE,
+    )
+    following = models.ForeignKey(
+        "User",
+        verbose_name=("Following"),
+        related_name="followings",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"{self.follower} follows to {self.following}"
+
+    class Meta:
+        verbose_name = "Follow"
+        verbose_name_plural = "Follows"
