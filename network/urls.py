061a9e4e9e7e98ec972c5573b5ab85716835ce8f
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-28 20:35:42 -0500

    feature: add path to react to post

--- network/urls.py
+++ network/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("edit-post/<int:post_id>", views.edit_post, name="edit-post"),
     path("profiles/<str:username>", views.profile, name="profiles"),
     path("following", views.following, name="following"),
+    path("react-post/<int:post_id>/", views.react_post, name="react-post"),
 ]
